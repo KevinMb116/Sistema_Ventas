eb9cb49643c551209ac4d04158890396e9dafd0d
KevinM <5717100644@utrng.edu.mx>
2024-07-05 21:45:57 -0600

    Lineas nuevas en Pruebas.cs

--- Prueba.cs
+++ Prueba.cs
@@ -1,1 +1,9 @@
 //Archivo de Prueba
+
+public class Pruebas
+{
+    public string Id { get; set; }
+    public string Prueba { get; set; }
+    public string Resultados { get; set; }
+    public string Datos { get; set; }
+}
