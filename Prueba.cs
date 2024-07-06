3d7bf728d937afb6e71069d2d0c66ce77500d3d9
KevinM <5717100644@utrng.edu.mx>
2024-07-05 21:49:36 -0600

    Lineas borradas en Pruebas.cs

--- Prueba.cs
+++ Prueba.cs
@@ -3,7 +3,5 @@
 public class Pruebas
 {
     public string Id { get; set; }
-    public string Prueba { get; set; }
     public string Resultados { get; set; }
-    public string Datos { get; set; }
 }
